e4495a718a90bb2063bb3a5f5726720b33e5c8de
norulshahlam <norulshahlam@gmail.com>
2021-12-19 22:21:54 +0800

    refactor

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # [Learn Mockito and JUnit - the most popular Java unit testing and mocking frameworks](https://github.com/in28minutes/MockitoTutorialForBeginners)
+# [Tutorial videos](https://cognizant.udemy.com/course/mockito-tutorial-with-junit-examples/learn/lecture/5678760#overview)
 
+The video tutorials starts from basic, then stubs, then mocks. Here the codes contains lesson from Mock onwards. There is no code on stubs as mock is the better implementation and stub is rarely used. Code examples starts from ListMockTest.class.
 
 run unit test cases in business package
 run unit test for uri in controller package
--- src/test/java/com/shah/unittest/unittesttutorials/business/ListMockTest.java
+++ src/test/java/com/shah/unittest/unittesttutorials/business/ListMockTest.java
@@ -15,6 +15,12 @@ Mockito verifies argument values in natural java style: by using an equals() met
 Mockito provides option to create spy on real objects. When spy is called, then actual method of real object is called.
 https://www.tutorialspoint.com/mockito/mockito_spying.htm
 
+The difference is that in mock, you are creating a complete mock or fake object while in spy, there is the real object and you just spying or stubbing specific methods of it.
+
+When using mock objects, the default behavior of the method when not stub is do nothing. Simple means, if its a void method, then it will do nothing when you call the method or if its a method with a return then it may return null, empty or the default value.
+
+While in spy objects, of course, since it is a real method, when you are not stubbing the method, then it will call the real method behavior. If you want to change and mock the method, then you need to stub it.
+
  */
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -143,14 +149,7 @@ class ListMockTest {
 		System.out.println(arrayListSpy.size());// 5
 
 		verify(arrayListSpy).add("Test4");
-		
-		/*
-		 The difference is that in mock, you are creating a complete mock or fake object while in spy, there is the real object and you just spying or stubbing specific methods of it.
 
-When using mock objects, the default behavior of the method when not stub is do nothing. Simple means, if its a void method, then it will do nothing when you call the method or if its a method with a return then it may return null, empty or the default value.
-
-While in spy objects, of course, since it is a real method, when you are not stubbing the method, then it will call the real method behavior. If you want to change and mock the method, then you need to stub it.
-		 */
 	}
 
 }
